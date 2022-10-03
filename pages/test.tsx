f0ad3f7c3360eb0485805a0fb58dee64e7a92387
Min-jae Kim <mink4661@snu.ac.kr>
2022-10-04 00:59:57 +0900

    css 설정

---
+++ pages/test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from '../styles/test.module.css';
+
+function Test(props) {
+    return <div className={classes.defaultclass}>하이</div>;
+}
+
+export default Test;
