2f4876695946d77ff3e203063f4f28f56c0c8ab1
Min-jae Kim <mink4661@snu.ac.kr>
2022-10-04 03:18:05 +0900

    user select box 만들기

--- pages/main/trade/index.tsx
+++ pages/main/trade/index.tsx
@@ -1,7 +1,12 @@
 function TradeHome(props) {
     return (
         <div>
-            <button>안녕</button>
+            <label htmlFor='user-list'>이름</label>
+            <select name='user-list' id='user-list'>
+                <option value='A'>A 유저</option>
+                <option value='B'>B 유저</option>
+                <option value='C'>C 유저</option>
+            </select>
         </div>
     );
 }
