aab9231515e2052ad934317ecfa849882357e8da
als8921 <als8921@naver.com>
2022-10-20 22:36:40 +0900

    메인페이지 만들 준비

---
+++ pages/main/index.tsx
@@ -0,0 +1,11 @@
+import classes from './index.module.css';
+
+function Main(props) {
+    return (
+        <div>
+            <h1>메인페이지 빨리 만들자</h1>
+        </div>
+    );
+}
+
+export default Main;
