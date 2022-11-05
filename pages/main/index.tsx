6470113ed6b6e364729136d66e16bc3aa48d0101
als8921 <als8921@naver.com>
2022-11-06 02:02:31 +0900

    상세페이지 만들기

--- pages/main/index.tsx
+++ pages/main/index.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import Image from 'next/image';
 import classes from './index.module.css';
+import testJson from '../../public/userTestData.json';
+
+function userInfo() {
+    return (
+        <div>
+            {testJson.map((user) => (
+                <div className={classes.content} key={user.id}>
+                    <div className={classes.infoName}>{user.name}</div>
+                    <div className={classes.infoSchool}>{user.school}</div>
+                    <div className={classes.info}>
+                        {user.avg}
+                        <div className={classes.infoTag}>Batting Average</div>
+                    </div>
+                    <div className={classes.info}>
+                        {user.hr}
+                        <div className={classes.infoTag}>Homeruns</div>
+                    </div>
+                    <div className={classes.info}>
+                        {user.rbi}
+                        <div className={classes.infoTag}>Runs Batted In</div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
+}
+
 export default function Main() {
     return (
         <body className={classes.background}>
@@ -19,6 +46,7 @@ export default function Main() {
                         ></Image>
                     </div>
                 </div>
+                <div> {userInfo()} </div>
             </div>
         </body>
     );
