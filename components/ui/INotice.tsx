8a32c336869c2cdf85406ea20cd3737b7cbb0dac
Min-jae Kim <mink4661@snu.ac.kr>
2022-11-06 23:40:26 +0900

    하단 바 개발중

---
+++ components/ui/INotice.tsx
@@ -0,0 +1,5 @@
+import classes from './INotice.module.css';
+
+export default function INotice() {
+    return <div className={classes.bar}>a</div>;
+}
---
+++ pages/test.tsx
@@ -0,0 +1,10 @@
+import INotice from '../components/ui/INotice';
+
+export default function Test() {
+    return (
+        <div>
+            <div>asdqww</div>
+            <INotice></INotice>
+        </div>
+    );
+}
