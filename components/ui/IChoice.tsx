a58185295f4cd8e9e54d4144407bc0d3de7f8b6f
junmok <42504239+wnstjd9288@users.noreply.github.com>
2022-11-10 01:44:23 +0900

    IChoice 개발 완료.

--- components/ui/IChoice.tsx
+++ components/ui/IChoice.tsx
@@ -1,15 +1,20 @@
 import classes from './IChoice.module.css';
-
+import {ReactNode} from 'react';
 interface Prop {
-    width: string;
-    height: string;
-    title: string;
+    width?: string;
+    height?: string;
+    title?: string;
+    children?: ReactNode;
+    onClickBtn: (any) => void;
 }
 
-const IChoice = ({width = '18rem', height = '13rem', title = '알림'}: Prop) => {
-    const onClick = () => {
-        console.log('asdf');
-    };
+const IChoice = ({
+    width = '18rem',
+    height = '13rem',
+    title = '알림',
+    children = '',
+    onClickBtn,
+}: Prop) => {
     return (
         <div>
             <div
@@ -17,12 +22,26 @@ const IChoice = ({width = '18rem', height = '13rem', title = '알림'}: Prop) =>
                 style={{width, height}}
             >
                 <div className={classes.titlebox}>{title}</div>
-                <div className={classes.textbox_container}>Context :</div>
+                <div className={classes.textbox}>{children}</div>
                 <div className={classes.btnbox}>
-                    <button className={classes.btn} onClick={onClick}>
-                        Btn A
+                    <button
+                        className={classes.btn}
+                        onClick={(e) => {
+                            onClickBtn(true);
+                        }}
+                        style={{backgroundColor: 'rgb(0,80,255)'}}
+                    >
+                        수락
                     </button>
-                    <button className={classes.btn}>Btn B</button>
+                    <button
+                        className={classes.btn}
+                        onClick={(e) => {
+                            onClickBtn(false);
+                        }}
+                        style={{backgroundColor: 'rgb(255,80,0)'}}
+                    >
+                        거절
+                    </button>
                 </div>
             </div>
         </div>
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,8 +1,14 @@
 import IChoice from '../../components/ui/IChoice';
+import IList from '../../components/ui/IList';
+import {useState} from 'react';
 function SignUp(props) {
+    const [bol, setBol] = useState<boolean>(true);
     return (
         <>
-            <IChoice></IChoice>
+            <IChoice width='20rem' onClickBtn={setBol} title='Tilte'>
+                <IList list={['asdf', 'asdgasg', 'gagadgad', 'a']}></IList>
+            </IChoice>
+            <div>{bol ? 'true' : 'false'}</div>
         </>
     );
 }
