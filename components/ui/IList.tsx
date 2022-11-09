a825f46d68b207f6fc010fc55e8e3ddcc8d99cd1
juicyaloe <mink4661@snu.ac.kr>
2022-11-09 23:54:56 +0900

    change div to frag

--- components/ui/IList.tsx
+++ components/ui/IList.tsx
@@ -1,13 +1,15 @@
+import {Fragment} from 'react';
+
 interface Prop {
     list: string[];
 }
 
 export default function IList({list}: Prop) {
     return (
-        <div>
+        <Fragment>
             {list.map((data, index) => (
                 <div key={index}>{data}</div>
             ))}
-        </div>
+        </Fragment>
     );
 }
