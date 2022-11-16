c97b74f1c92a5f879f54b2f6c4999a06ed5ce75b
Min-jae Kim <mink4661@snu.ac.kr>
2022-11-17 01:27:33 +0900

    test commit

--- pages/test.tsx
+++ pages/test.tsx
@@ -2,6 +2,6 @@ import {Fragment} from 'react';
 import IList from '../components/ui/IList';
 
 export default function Test() {
-    const dataList = ['dasd', 'dwd'];
+    const dataList = ['dasd', 'dwd', 'asd'];
     return <IList list={dataList}></IList>;
 }
