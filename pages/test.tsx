6b4cb4831792b2ad41d1bf4b16093dde901003f3
Min-jae Kim <mink4661@snu.ac.kr>
2022-11-19 17:51:13 +0900

    csstest

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,7 +1,13 @@
 import {Fragment} from 'react';
 import IList from '../components/ui/IList';
+import classes from './test.module.css';
 
 export default function Test() {
     const dataList = ['dasd', 'dwd', 'asd'];
-    return <IList list={dataList}></IList>;
+    return (
+        <Fragment>
+            <IList list={dataList}></IList>
+            <div className={classes.div}>qwdqw</div>
+        </Fragment>
+    );
 }
