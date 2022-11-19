684ae00ca8aee685f6b7a4bf1204b8ad5f6df4f4
Min-jae Kim <mink4661@snu.ac.kr>
2022-11-19 18:17:08 +0900

    info fetch 구현

--- pages/main/info/index.tsx
+++ pages/main/info/index.tsx
@@ -1,13 +1,29 @@
 import {Fragment} from 'react';
 import {useRouter} from 'next/router';
 import IList from '../../../components/ui/IList';
+import {useQuery} from '@tanstack/react-query';
+
+async function fetchMyInfo() {
+    let response = await fetch(
+        process.env.NEXT_PUBLIC_DOMAIN + 'api/users/profiles/my/',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Token ' + process.env.NEXT_PUBLIC_TOKEN,
+            },
+            body: JSON.stringify({
+                username: 'test1',
+            }),
+        },
+    );
+    return response.json();
+}
 
 export default function Info() {
-    const router = useRouter();
+    const {data, isLoading, isError, error} = useQuery(['myinfo'], fetchMyInfo);
 
-    return (
-        <Fragment>
-            <IList list={['123', '45', '이민철']}></IList>
-        </Fragment>
-    );
+    if (isLoading) return <div>로딩중</div>;
+
+    return <Fragment>{data.id}</Fragment>;
 }
