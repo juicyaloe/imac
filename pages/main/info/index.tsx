beb1989256e9d1635355b5e2d57c9fd3e46128f1
Min-jae Kim <mink4661@snu.ac.kr>
2022-11-19 18:57:58 +0900

    error 처리 고민중

--- pages/main/info/index.tsx
+++ pages/main/info/index.tsx
@@ -2,6 +2,19 @@ import {Fragment} from 'react';
 import {useRouter} from 'next/router';
 import IList from '../../../components/ui/IList';
 import {useQuery} from '@tanstack/react-query';
+import {Group} from 'next/dist/shared/lib/router/utils/route-regex';
+
+interface UserInfo {
+    id: number;
+    username: string;
+    nickname: string;
+    email: string;
+    players: {id: number; name: string}[];
+    selling: {id: number}[];
+    buying: {id: number}[];
+    budget: number;
+    data_joined: string;
+}
 
 async function fetchMyInfo() {
     let response = await fetch(
@@ -10,20 +23,26 @@ async function fetchMyInfo() {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                Authorization: 'Token ' + process.env.NEXT_PUBLIC_TOKEN,
+                Authorization: 'Token ',
             },
             body: JSON.stringify({
                 username: 'test1',
             }),
         },
     );
+    console.log(response.status.toString());
+    if (!response.ok) throw new Error(response.status.toString());
     return response.json();
 }
 
 export default function Info() {
-    const {data, isLoading, isError, error} = useQuery(['myinfo'], fetchMyInfo);
+    const {data, isLoading, isError, error} = useQuery<UserInfo, Error>(
+        ['myinfo'],
+        fetchMyInfo,
+    );
 
-    if (isLoading) return <div>로딩중</div>;
+    if (isLoading) return <div>qwdqwd</div>;
+    if (isError) return <div>{typeof error.message}</div>;
 
-    return <Fragment>{data.id}</Fragment>;
+    return <div>dqw</div>;
 }
